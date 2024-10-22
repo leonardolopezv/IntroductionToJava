3c5ffb566019f32de00e2b0b33656de309543536
leonardolopezv <leonardo.lv@globant.com>
2024-10-21 19:40:40 -0500

    Initial commit

---
+++ src/introduction/Main.java
@@ -0,0 +1,18 @@
+package introduction;
+
+public class Main {
+    //SINGLE LINE COMMENT
+    // Check java version: java -version
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+/* MULTIPLE LINE COMMENT
+ To Run the program...
+ C:\Users\Your Name>javac Main.java
+ C:\Users\Your Name>java Main
+ Output
+ Hello World
+ */
+
